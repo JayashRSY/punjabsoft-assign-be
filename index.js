37c04cd0c131f322548c4bcaa49decb837ba6889
Jayash Raj Singh Yadav <Jayash.Yadav@Geminisolutions.com>
2025-02-02 16:40:10 +0530

    updated cors

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ app.use(express.json());
 // Load student data from JSON
 const students = JSON.parse(fs.readFileSync("students.json", "utf-8"));
 
+const allowedOrigins = [
+    "https://punjabsoft-assign.netlify.app", // Deployed frontend
+    "http://localhost:4200",
+    "http://localhost:3000",
+    "http://localhost:5173" // Local development frontend
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}));
+
+
 // Search API
 app.get("/api/students", (req, res) => {
     const query = req.query.query?.toLowerCase() || "";
